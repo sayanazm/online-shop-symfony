e653a0d664cb9ed213d98e4e1c8fb5364839b4fa
sayanazm <kkit1303@gmail.com>
2025-04-23 15:02:29 +0300

    feat(migrations): add doctrine schema fixer and create first migration

--- migrations/Version20250423115741.php
+++ migrations/Version20250423115741.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250411113117 extends AbstractMigration
+final class Version20250423115741 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,28 +21,13 @@ final class Version20250411113117 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            DROP SEQUENCE user_id_seq CASCADE
-        SQL);
-        $this->addSql(<<<'SQL'
             CREATE TABLE "users" (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, second_name VARCHAR(255) DEFAULT NULL, phone_number INT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP TABLE "user"
         SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE SCHEMA public
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE TABLE "user" (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, second_name VARCHAR(255) DEFAULT NULL, phone_number INT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))
-        SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE "users"
         SQL);
